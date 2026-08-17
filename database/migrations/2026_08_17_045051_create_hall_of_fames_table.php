adeb9541123643fcb9ad18330d962968b390fedc
RNJey <miranyuliastuti@gmail.com>
2026-08-17 12:19:53 +0700

    mempersiapkan migrate table

--- database/migrations/2026_08_17_045051_create_hall_of_fames_table.php
+++ database/migrations/2026_08_17_045051_create_hall_of_fames_table.php
@@ -9,13 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('hall_of_fames', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('hall_of_fames', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('role_title');
+        $table->string('sub_group_name')->nullable();
+        $table->string('image_path');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
